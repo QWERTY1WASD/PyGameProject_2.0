d9842a79a516322bbf5bd28b70184a837767209a
il060 <il06.03.77@gmail.com>
2023-01-04 15:30:51 +0500

    Its Working

---
+++ main.py
@@ -0,0 +1,95 @@
+import pygame
+from utils import load_image, terminate, generate_level, load_level
+from startscreen import start_screen
+from camera import Camera
+
+FPS = 50
+SIZE = WIDTH, HEIGHT = 1260, 720
+TILE_WIDTH = TILE_HEIGHT = 50  # Todo: sprites size = 50 or 100
+BACKGROUND_COLOR = (0, 0, 0)
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption('BattleFront')
+player = None
+clock = pygame.time.Clock()
+
+
+tile_images = {
+    'ground': load_image('grass.png'),
+    'barrier': load_image('barrier.png'),
+    'infantry': load_image('infantry.png'),
+    'infantry_enemy': load_image('infantry_enemy.png')
+}
+obstacles = {'barrier'}
+player_image = load_image('point.png')
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y, vars):
+        super().__init__(vars['tiles_group'], vars['all_sprites'])
+        if tile_type in obstacles:
+            vars['obstacles_group'].add(self)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            TILE_WIDTH * pos_x, TILE_HEIGHT * pos_y)
+
+
+class PivotPoint(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, vars):
+        super().__init__(vars['player_group'], vars['all_sprites'])
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            TILE_WIDTH * pos_x + (TILE_WIDTH - self.image.get_width()) // 2,
+            TILE_HEIGHT * pos_y + (TILE_HEIGHT - self.image.get_height()) // 2
+        )
+
+
+def main():
+    global player, level_x, level_y, all_sprites, tiles_group, player_group, obstacles_group
+
+    all_sprites = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    obstacles_group = pygame.sprite.Group()
+    start_screen(screen)
+    # filename = input('Введите название уровня: ')
+    filename = 'map.txt'
+    player, level_x, level_y = generate_level(load_level(filename), globals())
+    camera = Camera(WIDTH, HEIGHT)
+    camera.add(player)
+    camera.add_group(tiles_group)
+    is_moving = False
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                is_moving = True
+            elif event.type == pygame.KEYUP:
+                is_moving = False
+        if is_moving:
+            old_player_rect = player.rect.copy()
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]:
+                player.rect.x -= TILE_WIDTH
+            elif key[pygame.K_RIGHT]:
+                player.rect.x += TILE_WIDTH
+            elif key[pygame.K_UP]:
+                player.rect.y -= TILE_HEIGHT
+            elif key[pygame.K_DOWN]:
+                player.rect.y += TILE_HEIGHT
+            if pygame.sprite.spritecollideany(player, obstacles_group):
+                player.rect = old_player_rect
+        screen.fill(BACKGROUND_COLOR)
+        # изменяем ракурс камеры
+        camera.update(player)
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+    terminate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import pygame
+
+
+BARRIER = '#'
+GROUND = '0'
+CAMERA_POINT = '@'
+INFANTRY = 'I'
+INFANTRY_ENEMY = 'K'
+TANK = 'T'
+TANK_ENEMY = 'G'
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', 'images', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        terminate()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_level(filename):
+    filename = os.path.join('data', 'levels', filename)
+    if not os.path.isfile(filename):
+        print(f"Файл с картой уровня '{filename}' не найден")
+        terminate()
+    with open(filename, 'r') as mapFile:
+        level_map = [line.rstrip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level, vars):
+    from main import Tile, PivotPoint
+    h = len(level)
+    w = len(level[0])
+    new_player, x, y = None, None, None
+    for y in range(h):
+        for x in range(w):
+            if level[y][x] == GROUND:
+                Tile('ground', x, y, vars)
+            elif level[y][x] == BARRIER:
+                Tile('barrier', x, y, vars)
+            elif level[y][x] == CAMERA_POINT:
+                Tile('ground', x, y, vars)
+                new_player = PivotPoint(x, y, vars)
+            # elif level[y][x] == TANK:
+            #    Tile('ground', x, y, vars)
+            #    new_player = PivotPoint(x, y, vars)
+    return new_player, w, h
